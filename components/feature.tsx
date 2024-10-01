7eedafb4540f5bd60fa0df94fc0d6f372bf2cb4d
prince-2003 <prince_22410@aitpune.edu.in>
2024-10-01 23:35:12 +0530

    pricing section done

--- components/feature.tsx
+++ components/feature.tsx
@@ -5,6 +5,8 @@ import { TbBoxMultiple } from "react-icons/tb";
 import { SiGooglegemini } from "react-icons/si";
 import { FiRefreshCw } from "react-icons/fi";
 import { FaLaptopCode } from "react-icons/fa";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 const features = [
   {
@@ -65,65 +67,51 @@ export default function Feature() {
   }, [features]);
 
   return (
-    <div id="features" className="w-[96vw] m-2 rounded-xl  p-10 md:p-20 bg-gray-100">
-      
-        <div className="flex flex-col justify-center gap-10 md:gap-20">
-          <div className="text-2xl md:text-4xl flex flex-col gap-2 font-bold text-start">
-            <h2>Everything Your Need to Learn to Code</h2>
-            <p className="text-sm font-light">
-              Socrates exceptional flexibility can handle any problems in
-              programming. And we never stop innovating.
-            </p>
-          </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
-  {features.map((feature, index) => (
     <div
-      key={index}
-      className="flex flex-col gap-4 items-center transform transition-transform duration-300 group hover:scale-105 "
+      id="features"
+      className="w-[96vw] m-2 rounded-xl  p-10 md:p-20 bg-gray-100"
     >
-      <div className="p-4 grayscale bg-gray-100 rounded-lg group-hover:grayscale-0  shadow-lg overflow-hidden">
-        <img
-          src={"/assets/placeholder.png"}
-          className="rounded-lg "
-        />
+      <div className="flex flex-col justify-center gap-10 md:gap-20">
+        <div className="text-2xl md:text-4xl flex flex-col gap-2 font-bold text-start">
+          <div className="inline-flex gap-1 items-center bg-black/5 bg-opacity-10 backdrop-filter backdrop-blur-lg rounded-xl px-4 py-2 text-black text-[.8rem] md:text-[1rem] max-w-max">
+            Features
+            <span className="bg-black/5 bg-opacity-10 backdrop-filter backdrop-blur-lg rounded-md px-2 text-black">
+              <FontAwesomeIcon icon={faArrowRight} />
+            </span>
+          </div>
+          <h2>Everything Your Need to Learn to Code</h2>
+          <p className="text-sm font-light">
+            Socrates exceptional flexibility can handle any problems in
+            programming. And we never stop innovating.
+          </p>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-10 ">
+          {features.map((feature, index) => (
+            <div
+              key={index}
+              className="flex flex-col gap-2 md:gap-4 items-center transform transition-transform duration-300 group hover:scale-105 "
+            >
+              <div className="p-2 md:p-4 grayscale bg-gray-100 rounded-lg group-hover:grayscale-0  shadow-lg overflow-hidden">
+                <img src={"/assets/placeholder.png"} className="rounded-lg " />
+              </div>
+              <div
+                ref={(el) => {
+                  cardRefs.current[index] = el;
+                }}
+                className="bg-white flex flex-col group-hover:transition-shadow group-hover:shadow-lg justify-center rounded-lg p-4"
+                style={{ minHeight: maxHeight }}
+              >
+                <h3 className="text-xl md:text-2xl font-bold">
+                  {feature.title}
+                </h3>
+                <p className="text-sm font-light text-start">
+                  {feature.shortDescription}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
-      <div
-        ref={(el) => {
-          cardRefs.current[index] = el;
-        }}
-        className="bg-white flex flex-col group-hover:transition-shadow group-hover:shadow-lg justify-center rounded-lg p-4"
-        style={{ minHeight: maxHeight }}
-      >
-        <h3 className="text-xl md:text-2xl font-bold">{feature.title}</h3>
-        <p className="text-sm font-light text-start">{feature.shortDescription}</p>
-      </div>
-    </div>
-  ))}
-</div>
-      </div>
-      <ul className="flex flex-col md:flex-row w-full max-w-2xl justify-center items-center gap-6 py-4 z-10">
-        <li className="w-full md:w-auto">
-          <Link href="/auth" passHref>
-            <Button className="w-full hover:scale-105 transition-transform">
-              Login/Sign Up
-            </Button>
-          </Link>
-        </li>
-        <li className="w-full md:w-auto">
-          <Link href="/submit-problems" passHref>
-            <Button className="w-full hover:scale-105 transition-transform">
-              Submit a New Problem
-            </Button>
-          </Link>
-        </li>
-        <li className="w-full md:w-auto">
-          <Link href="/problems" passHref>
-            <Button className="w-full hover:scale-105 transition-transform">
-              Solve Problems
-            </Button>
-          </Link>
-        </li>
-      </ul>
     </div>
   );
 }

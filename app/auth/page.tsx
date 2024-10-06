41479a929872e11845438ba86c021f45d5521d7e
Prince <prince_22410@aitpune.edu.in>
2024-10-06 15:31:39 +0530

    session implementation

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -25,7 +25,15 @@ export default function LoginPage() {
   const handleSubmit = async (email: string, password: string) => {
     try {
       if (isLogin) {
-        await signInWithEmailAndPassword(auth, email, password);
+        const userCredential = await signInWithEmailAndPassword(auth, email, password);
+        const idToken = await userCredential.user.getIdToken();
+        console.log("idToken", idToken);
+        await fetch('http://localhost:8900/session_login', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ idToken }),
+          credentials: 'include', 
+        });
       } else {
         await createUserWithEmailAndPassword(auth, email, password);
       }
